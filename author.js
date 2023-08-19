cadbfc3e200fe7382bd8e2bc78d42c464610dfd8
misba0770 <80534061+misba0770@users.noreply.github.com>
2023-08-19 21:27:43 +0530

    Add files via upload

---
+++ author.js
@@ -0,0 +1,19 @@
+const images = [
+    {
+      src: "image1.jpg",
+      alt: "Image 1",
+      caption: "This is the first image.",
+    },
+    {
+      src: "image2.jpg",
+      alt: "Image 2",
+      caption: "This is the second image.",
+    },
+    {
+      src: "image3.jpg",
+      alt: "Image 3",
+      caption: "This is the third image.",
+    },
+  ];
+
+  
